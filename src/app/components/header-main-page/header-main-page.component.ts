f5c75d30387c31265fcf16d03dc50dae91518ff5
TheGentleDucky <owenscruzsean@gmail.com>
2025-04-07 18:00:02 +0200

    Added: Main Page, Main Page Header and Sign In Component

---
+++ src/app/components/header-main-page/header-main-page.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-header-main-page',
+  imports: [],
+  templateUrl: `./header-main-page.component.html`,
+  styleUrl: './header-main-page.component.css',
+})
+
+export class HeaderMainPageComponent {
+  imagen = "/assets/asps.png";
+}
---
+++ src/app/components/header-sign-in/header-sign-in-sign-up.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-header-sign-in',
+  imports: [],
+  templateUrl: `./header-sign-in-sign-up.component.html`,
+  styleUrl: './header-sign-in-sign-up.component.css',
+})
+
+export class HeaderSignUpComponent {
+  imagen="/assets/asps.png";
+}
